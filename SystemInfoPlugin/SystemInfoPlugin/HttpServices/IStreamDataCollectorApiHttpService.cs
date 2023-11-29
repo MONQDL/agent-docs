4282d3950b6ea11b990b4d2a721bdab6a51cda50
kozhenovis <kozhenovis@monqlab.com>
2023-11-29 17:07:20 +0300

    Добавлен проект для статьи "Создание плагина для отправки логов (событийной информации)"

---
+++ SystemInfoPlugin/SystemInfoPlugin/HttpServices/IStreamDataCollectorApiHttpService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace SystemInfoPlugin.HttpServices;
+
+/// <summary>
+/// The interface of a service that operates with Data Stream API.
+/// </summary>
+public interface IStreamDataCollectorApiHttpService
+{
+    /// <summary>
+    /// Send events or logs to Monq.
+    /// </summary>
+    /// <param name="baseUri">The base URI of the Monq system.</param>
+    /// <param name="streamKey">The key of the target data stream.</param>
+    /// <param name="userspaceId">The identifier of the target userspace.</param>
+    /// <param name="events">A collection of events or logs to send.</param>
+    public Task PushEvents(string baseUri, string streamKey, long userspaceId, IList events);
+}
---
+++ SystemInfoPlugin/SystemInfoPlugin/HttpServices/Implementation/StreamDataCollectorApiHttpService.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SystemInfoPlugin.HttpServices.Implementation;
+
+/// <summary>
+/// The service that operates with Data Stream API.
+/// </summary>
+public class StreamDataCollectorApiHttpService : IStreamDataCollectorApiHttpService
+{
+    const string UserspaceHeader = "X-Smon-Userspace-Id";
+    const string StreamDataApiUri = "api/public/cl/v1/stream-data";
+    static readonly TimeSpan _defaultTimeout = TimeSpan.FromSeconds(30);
+    static readonly JsonSerializerOptions _serializerOptions = new()
+    {
+        WriteIndented = true,
+        PropertyNameCaseInsensitive = true,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+    };
+    readonly IHttpClientFactory _httpClientFactory;
+
+    /// <summary>
+    /// The service's constructor.
+    /// </summary>
+    /// <param name="httpClientFactory">A factory that creates HttpClients</param>
+    public StreamDataCollectorApiHttpService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    /// <inheritdoc/>
+    public async Task PushEvents(string baseUri, string streamKey, long userspaceId, IList events)
+    {
+        if (events.Count == 0)
+            return;
+        var json = JsonSerializer.Serialize(events, _serializerOptions);
+        var data = new StringContent(json, Encoding.UTF8, Application.Json);
+
+        using var client = _httpClientFactory.CreateClient();
+        client.BaseAddress = new Uri(baseUri);
+        client.DefaultRequestHeaders.TryAddWithoutValidation(UserspaceHeader, userspaceId.ToString());
+
+        var requestUri = $"{StreamDataApiUri}?streamKey={streamKey}";
+        var response = await client.PostAsync(requestUri, data, new CancellationTokenSource(_defaultTimeout).Token);
+        response.EnsureSuccessStatusCode();
+    }
+}
